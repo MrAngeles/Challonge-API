091112efbd02db1ccb92690c9cdfb7c51bcc14c4
bendeloyola <bendeloyola2017@gmail.com>
2021-07-28 19:03:20 +0800

    fix error get API

--- src/axios/AxiosInstance.js
+++ src/axios/AxiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'https://api.challonge.com/v2/',
     headers: {
+        "Content-type": "application/vnd.api+json",
         "Accept": "application/json",
         "Authorization-Type": "v1",
         // "Authorization": process.env.REACT_APP_CHALLONGE_API_KEY
